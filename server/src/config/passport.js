0a4e8ca1036ca51a67ebe88054f530b52da80837
rupak choppala <rupakchoppala@gmail.com>
2025-04-29 22:12:52 +0530

    deplyed changes

--- server/src/config/passport.js
+++ server/src/config/passport.js
@@ -11,7 +11,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL:"/auth/google/callback",
+      callbackURL:"https://unitedngos-1.onrender.com/auth/google/callback",
       scope: ['profile', 'email'],
       passReqToCallback: true,
       prompt: 'select_account' 
