9151745a2b3d1c94c0999a64c2886f599865d57d
rupak choppala <rupakchoppala@gmail.com>
2025-05-08 10:39:30 +0530

    pics added for readme

--- client/src/components/pages/UpcomingProjects.jsx
+++ client/src/components/pages/UpcomingProjects.jsx
@@ -38,7 +38,7 @@ const UpcomingProjects = () => {
             <motion.div
               key={project.id}
               whileHover={{ scale: 1.02 }}
-              className="bg-white w-[250px] h-[300px] md:h-[450px] md:w-[350px] rounded-2xl shadow-lg p-6 flex flex-col justify-between hover:shadow-xl transition-shadow duration-300"
+              className="bg-white w-[250px] h-[400px] md:h-[450px] md:w-[350px] rounded-2xl shadow-lg p-6 flex flex-col justify-between hover:shadow-xl transition-shadow duration-300"
             >
               <div className="flex justify-center items-center h-auto w-auto">
                 <img src="logo.png" alt="logo" 
